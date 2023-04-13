554bde7517d52f8e0d6d8b25155420efdbf7acbc
Admin <Admin@DESKTOP-DV5LFHQ>
2023-04-13 12:25:58 +0530

    commit by sharan

--- src/test/java/Vtiger/Practise/Pract.java
+++ src/test/java/Vtiger/Practise/Pract.java
@@ -3,12 +3,19 @@ package Vtiger.Practise;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import io.opentelemetry.exporter.logging.SystemOutLogExporter;
+
 public class Pract {
 @Test
 public void sam()
 {
-	Assert.fail();
+	
 	System.out.println("bye");
+	
+	
+	System.out.println("this is sharan");
+	System.out.println("how are you");
+	
 }
 	
 }
