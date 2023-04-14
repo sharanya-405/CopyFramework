a8ef86aac4a6696c35e32ded10d7a215cbaa3108
Admin <Admin@192.168.198.1>
2023-04-14 11:13:41 +0530

    commit by jr

--- src/test/java/Vtiger/Practise/Sample.java
+++ src/test/java/Vtiger/Practise/Sample.java
@@ -1,5 +1,11 @@
 package Vtiger.Practise;
 
-public class Sample {
+import org.testng.annotations.Test;
 
+public class Sample {
+  @Test
+  public void ten()
+  {
+	  System.out.println("haaaaaaaaaaaaiiiiiiiiii");
+  }
 }
